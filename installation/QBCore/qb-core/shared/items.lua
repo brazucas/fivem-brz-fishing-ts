2cc47614a0b175cc7e5485b4e710716cf31a54f2
pedropapa <pedro.papadopolis@gmail.com>
2024-09-28 23:35:25 +1000

    feat: update qbcore installation docs

--- installation/QBCore/qb-core/shared/items.lua
+++ installation/QBCore/qb-core/shared/items.lua
@@ -3,7 +3,7 @@
 -- Do not replace all your QBShared.Items object, just add the items below to your file.
 
 QBShared.Items = {
-    -- brz-fishing
+    -- Copy the items from here
     ['fishingrod1'] = {
         ['name'] = 'fishingrod1',
         ['label'] = 'Fishing Rod',
@@ -16,7 +16,7 @@ QBShared.Items = {
         ['combinable'] = nil,
         ['description'] = 'Common fishing rod'
     },
-    ['commonbait'] = {
+    ['commonbait']  = {
         ['name'] = 'commonbait',
         ['label'] = 'Common bait',
         ['weight'] = 1,
@@ -27,6 +27,90 @@ QBShared.Items = {
         ['shouldClose'] = false,
         ['combinable'] = true,
         ['description'] = 'Common bait. Can be found at any fishing store'
-    }
-    -- ...
+    },
+    ['fish']        = {
+        ['name'] = 'fish',
+        ['label'] = 'Common Fish',
+        ['weight'] = 5,
+        ['type'] = 'item',
+        ['image'] = 'fish.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Common fish that can be found in any fishing spots'
+    },
+    ['dolphin']     = {
+        ['name'] = 'dolphin',
+        ['label'] = 'Dolphin',
+        ['weight'] = 50,
+        ['type'] = 'item',
+        ['image'] = 'dolphin.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Uncomon fish that can be found in the deep sea'
+    },
+    ['hammershark'] = {
+        ['name'] = 'hammershark',
+        ['label'] = 'Hammer Shark',
+        ['weight'] = 50,
+        ['type'] = 'item',
+        ['image'] = 'hammershark.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Uncomon fish that can be found in the deep sea'
+    },
+    ['tigershark']  = {
+        ['name'] = 'tigershark',
+        ['label'] = 'Tiger Shark',
+        ['weight'] = 50,
+        ['type'] = 'item',
+        ['image'] = 'tigershark.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Uncomon fish that can be found in the deep sea'
+    },
+    ['killerwhale'] = {
+        ['name'] = 'killerwhale',
+        ['label'] = 'Killer Whale',
+        ['weight'] = 50,
+        ['type'] = 'item',
+        ['image'] = 'killerwhale.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Epic fish that can be found in deep sea at certain seasons of the year'
+    },
+    ['humpback']    = {
+        ['name'] = 'humpback',
+        ['label'] = 'Humpback',
+        ['weight'] = 200,
+        ['type'] = 'item',
+        ['image'] = 'humpback.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Legendary fish that can be found in deep sea at certain seasons of the year'
+    },
+    ['stingray']    = {
+        ['name'] = 'stingray',
+        ['label'] = 'Stingray',
+        ['weight'] = 20,
+        ['type'] = 'item',
+        ['image'] = 'stingray.png',
+        ['unique'] = false,
+        ['useable'] = false,
+        ['shouldClose'] = false,
+        ['combinable'] = true,
+        ['description'] = 'Rare fish that can be found in deep sea at certain seasons of the year'
+    },
+    -- To here
 }
