bef0aac9275aa724bb5600fe1f536523fe0a2ae1
pedropapa <pedro.papadopolis@gmail.com>
2025-06-24 21:38:18 +1000

    feat: add water splash effects in fishing hotspots

--- client/bootstrap/create-hotspots.ts
+++ client/bootstrap/create-hotspots.ts
@@ -1,4 +1,4 @@
-import { FishingHotspots } from "@/types/hotspots";
+import { FishingHotspot, FishingHotspots } from "@/types/hotspots";
 import { createBlip } from "@brz-fivem-sdk/client/services/blips";
 import { t } from "@config/locales";
 
@@ -10,26 +10,69 @@ const BLIP_DEFAULT_SCALE = 0.7;
 const BLIP_DEFAULT_DISPLAY = 4;
 const BLIP_DEFAULT_SHORT_RANGE = true;
 
+const NUMBER_OF_SPLAHES = 4;
+
 const createBlipsFromSettings = (
-  hotspots: FishingHotspots,
+  id: string,
+  hotspot: FishingHotspot,
   blipSettings: any = {}
 ) => {
-  validateSettings(blipSettings);
   const blipLabel = t("fishing_hotspot_blip_label");
 
-  for (const [id, hotspot] of Object.entries(hotspots)) {
-    createBlip({
-      id,
-      radius: hotspot.radius || 50.0,
-      label: blipLabel,
-      coords: [hotspot.coords.x, hotspot.coords.y, hotspot.coords.z],
-      sprite: blipSettings.sprite || BLIP_DEFAULT_SPRITE,
-      scale: blipSettings.scale || BLIP_DEFAULT_SCALE,
-      display: blipSettings.display || BLIP_DEFAULT_DISPLAY,
-      color: blipSettings.color || BLIP_DEFAULT_COLOR,
-      shortRange: blipSettings.shortRange || BLIP_DEFAULT_SHORT_RANGE,
-    });
+  createBlip({
+    id,
+    radius: hotspot.radius || 50.0,
+    label: blipLabel,
+    coords: [hotspot.coords.x, hotspot.coords.y, hotspot.coords.z],
+    sprite: blipSettings.sprite || BLIP_DEFAULT_SPRITE,
+    scale: blipSettings.scale || BLIP_DEFAULT_SCALE,
+    display: blipSettings.display || BLIP_DEFAULT_DISPLAY,
+    color: blipSettings.color || BLIP_DEFAULT_COLOR,
+    shortRange: blipSettings.shortRange || BLIP_DEFAULT_SHORT_RANGE,
+  });
+};
+
+const cycleWaterSplashEffects = (hotspot: FishingHotspot) => {
+  if (isPlayerNearHotspot(GetEntityCoords(PlayerPedId(), true), hotspot)) {
+    for (let i = 0; i < NUMBER_OF_SPLAHES; i++) {
+      modifyWaterWithCoordsFuzziness([hotspot.coords.x, hotspot.coords.y]);
+    }
   }
+
+  const timerWithFuzziness = 300 + Math.random() * 100;
+
+  setTimeout(() => {
+    cycleWaterSplashEffects(hotspot);
+  }, timerWithFuzziness);
+};
+
+const modifyWaterWithCoordsFuzziness = (coords: number[]) => {
+  const fuzziness = Math.random() * 5 + 0.05;
+  const heightWithFuzziness = 1.0;
+
+  ModifyWater(
+    coords[0] + fuzziness,
+    coords[1] + fuzziness,
+    heightWithFuzziness,
+    20.0
+  );
+};
+
+const isPlayerNearHotspot = (
+  coords: number[],
+  hotspot: FishingHotspot
+): boolean => {
+  const playerFieldOfView = GetGameplayCamFov();
+  const distance = GetDistanceBetweenCoords(
+    coords[0],
+    coords[1],
+    coords[2],
+    hotspot.coords.x,
+    hotspot.coords.y,
+    hotspot.coords.z,
+    true
+  );
+  return distance <= hotspot.radius + playerFieldOfView * 2;
 };
 
 const validateSettings = (settings?: FishingHotspots | undefined): boolean => {
@@ -51,5 +94,10 @@ const validateSettings = (settings?: FishingHotspots | undefined): boolean => {
 };
 
 if (validateSettings(SETTINGS.hotspots)) {
-  createBlipsFromSettings(SETTINGS.hotspots, SETTINGS.blipSettings);
+  for (const [id, hotspot] of Object.entries(
+    SETTINGS.hotspots as FishingHotspots
+  )) {
+    createBlipsFromSettings(id, hotspot, SETTINGS.blipSettings);
+    cycleWaterSplashEffects(hotspot);
+  }
 }
